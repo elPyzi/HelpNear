a912a8c611767ad40ca4d18824bffd44a7bd9678
Danila Pyzhikov <danila.pyzhikov@gmail.com>
2025-04-12 17:00:11 +0300

    feat: Cookie component

--- HelpNear-client/src/layouts/Layout.tsx
+++ HelpNear-client/src/layouts/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
+import Cookie from '@/components/Cookie/Cookie';
 
 const Layout = () => {
   return (
@@ -10,6 +11,8 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
+      <Cookie />
+
       <Footer />
     </>
   );
